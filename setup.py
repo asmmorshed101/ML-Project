7e62bf1eff829d0a550bc7717a6f5a7c7ddf377e
asmmorshed101 <asmmorshed101@gmail.com>
2026-03-12 14:22:47 +0600

    setup

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+from typing import List
+
+def get_requirements(file_path:str)->List[str]:
+    '''This function will return the list of requirements'''
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace('\n','') for req in requirements]
+
+setup(
+    name='ML-Project',
+    version='0.1',
+    author='Morshed',
+    author_email='asmmorshed101@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt'),
+    
+)
